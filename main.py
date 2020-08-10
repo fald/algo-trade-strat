a1dd6fee1ca1285bdb586c676de10283d0454caf
Firas <firasaldabbas+git@gmail.com>
2020-08-10 19:23:55 -0400

    Plot (and added) adjusted closing prices for sample data

--- main.py
+++ main.py
@@ -7,5 +7,18 @@ import numpy as np
 from datetime import datetime
 import matplotlib.pyplot as plt
 
-
 plt.style.use('fivethirtyeight')
+figsize = (12.5, 4.5)
+plt.figure(figsize=figsize)
+
+filename = "AAPL.csv"
+
+aapl = pd.read_csv(filename)
+
+plt.plot(aapl['Adj Close'], label="AAPL")
+plt.title("Apple's adjusted closing price history")
+plt.xlabel("29 Sept 2014 - 29 Mar 2018")
+plt.ylabel("Adj. close prices - USD")
+plt.legend(loc="upper left")
+plt.show()
+
