56195a3fa07e267984e1da695e76be3c2fc0670f
Firas <firasaldabbas+git@gmail.com>
2020-08-10 19:05:02 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# Description:
+# This program uses the dual moving average crossover to determine
+# buy and sell points of stock.
+
+import pandas as pd
+import numpy as np
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+
+plt.style.use('fivethirtyeight')
